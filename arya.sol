989cb3fcd4e2aad5402bbae5e409cb3cab3dce15
aryarocx2004 <144051136+aryarocx2004@users.noreply.github.com>
2023-09-04 22:46:37 +0530

    Update arya.sol

--- arya.sol
+++ arya.sol
@@ -10,19 +10,19 @@ string public tokenAbbriv = "ARY";
 uint public totalSupply = 0;
 
 
-mapping(address => uint) public _balance_;
+mapping(address => uint) public balances;
 
 
 function mint (address addrr_, uint val_) public {
     totalSupply += val_;
-    _balance_[addrr_] += val_;
+    balances[addrr_] += val_;
 }
 
 
 function burn (address addrr_, uint val_) public {
-    if(_balance_[addrr_] >= val_){
+    if(balances[addrr_] >= val_){
         totalSupply -= val_;
-        _balance_[addrr_] -= val_;
+        balances[addrr_] -= val_;
     }
     
 }
